4125e891e2a6f1b8508a53df1696296899ec1a68
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-07-29 17:32:27 -0400

    custom bottom navigation bar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: _HomeView());
+    return const Scaffold(body: _HomeView(), bottomNavigationBar: CustomBottomNavigation(),);
   }
 }
 
