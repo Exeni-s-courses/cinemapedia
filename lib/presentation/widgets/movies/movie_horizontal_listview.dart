8a96cb5377d6ac26cd4dfe12411c2165e867de3c
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-07-30 21:18:32 -0400

    human formats class

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -84,20 +85,23 @@ class _Slide extends StatelessWidget {
             child: Text(movie.title, maxLines: 2, style: textStyles.titleSmall),
           ),
 
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-              const SizedBox(width: 3),
-              Text(
-                '${movie.voteAverage}',
-                style: textStyles.bodyMedium!.copyWith(
-                  color: Colors.yellow.shade800,
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 3),
+                Text(
+                  '${movie.voteAverage}',
+                  style: textStyles.bodyMedium!.copyWith(
+                    color: Colors.yellow.shade800,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 3),
-
-              Text('${movie.popularity}', style: textStyles.bodySmall),
-            ],
+                // const SizedBox(width: 3),
+                const Spacer(),
+                Text(HumanFormats.number(movie.popularity), style: textStyles.bodySmall)
+              ],
+            ),
           ),
         ],
       ),
